17ed5031b568c46f3d976479f23cda40880c2a87
Emile De Wilde <synaptic4u@gmail.com>
2024-09-05 15:20:06 +0200

    FormParser.

--- js/FormParser/FormParser.js
+++ js/FormParser/FormParser.js
@@ -2,38 +2,34 @@ import { Response } from "../Response/Response.js";
 
 const FormParser = {
     'error': 0,
-    'response': Response,
-    'fields': {
-    },
+    'response': {},
+    'fields': {},
     'parse': function (form){
 
         if(form.elements.length === 0){
             this.error = 1;
             this.response.error = this.error;
             this.response.message = '<span class="error">Unable to parse form fields.<br>Please constact support.</span>';
+            
             return this.response;
         }
 
         for (const key in form.elements) {
-            console.log(object[key]);
 
             switch (true) {
-                case (form.elements[key.type] === "input"):
-                    this.fields.
+                case (form.elements[key].type === "input"):
+                    this.fields[form.elements[key].name] = form.elements[key].value;
                     break;
-                case (form.elements[key.type] === "textarea"):
-                
+                case (form.elements[key].type === "textarea"):
+                    this.fields[form.elements[key].name] = form.elements[key].value;
                 break;
 
-                case (form.elements[key.type] === "xxxx"):
-                
+                case (form.elements[key].type === "xxxx"):
+                    this.fields[form.elements[key].name] = form.elements[key].value;
                 break;
-            
-                default:
-                    break;
             }
         }
-        return fields;
+        return this.fields;
     }
 };
 
